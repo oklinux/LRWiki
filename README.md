8d4ccd03428c6ee5f84ba7aa6a8213f9b54d1347
Holegots <fuergaosi@gmail.com>
2019-02-01 15:32:33 +0800

    updata READEME and add requirement

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # linuxren wiki website
-## technology
-back-end:django+drf
-front-end:vue 
-database:mysql
+后端:`Django`+`DRF`  
+前端:`vue`  
+数据库:`mysql`  
+## 介绍
+为社区所做的简单的`wiki`站点的  
+使用`蝉知`的`User`表(需要建立一个视图  
+使用`jwt`来代替`session`  
--- api/views.py
+++ api/views.py
@@ -3,16 +3,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Doc
-from .serializers import Docserializers
+from .serializers import DocSerializers
+
 # Create your views here.
 
 
 class DocView(APIView):
-
     def get(self, request):
-        Docs=Doc.objects.all()
-        serializers = Docserializers(Docs, many=True)
-        response=[]
+        """
+        return all docs(title,account,data_time,update_time)
+        """
+        Docs = Doc.objects.all()
+        serializers = DocSerializers(Docs, many=True)
+        response = []
         for x in serializers.data:
             x.pop('text')
             response.append(x)
